0e1c75dd52f64674a9a7dd5379af7693d10c8c13
Ricardo González Pacheco <rigonpa@gmail.com>
2020-10-20 20:10:44 +0200

    wrong dependency version of Room. Solved

--- app/src/main/java/com/example/nasaapp/data/model/Patent.kt
+++ app/src/main/java/com/example/nasaapp/data/model/Patent.kt
@@ -3,7 +3,6 @@ package com.example.nasaapp.data.model
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import java.io.Serializable
-import com.google.gson.annotations.SerializedName
 
 @Entity(tableName = "patent_table")
 data class Patent(
